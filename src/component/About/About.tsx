70d43b19322c956e135f662b8f8a732d969931ea
aquibAhmad <aquibahamd.vb@ensuredit.com>
2023-04-13 22:27:11 +0530

    added aboyt me section

--- src/component/About/About.tsx
+++ src/component/About/About.tsx
@@ -1,9 +1,59 @@
-import React from 'react'
+/* eslint-disable react/no-unescaped-entities */
+import { Box, Chip, Divider, Typography } from '@mui/material';
+const skills = ["HTML",  "CSS",  "JavaScript",  "React",  "SASS",  "GIT",  "Github",  "Responsive Design",  "SEO",  "Terminal",  "Express Basics",  "SQL Basics"];
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <Box>
+      <Box sx={{ pt: 5, width: '100%' }}>
+        <Typography
+          variant='h1'
+          textAlign={'center'}
+          sx={{ textTransform: 'uppercase' }}>
+          About ME
+        </Typography>
+        <Divider
+          sx={{ height: '0.2rem', mx: { xs: 5, md: 25 }, background: 'black' }}
+        />
+        <Box sx={{ py: 4, display: 'flex', justifyContent: 'center' }}>
+          <Typography variant='h3' textAlign={'center'} sx={{ width: '70rem' }}>
+            Here you will find more information about me, what I do, and my
+            current skills mostly in terms of programming and technology
+          </Typography>
+        </Box>
+      </Box>
+      <Box sx={{ display: 'flex' ,flexDirection: { xs: 'column', sm: 'row' } }}>
+        <Box sx={{flex:1}}>
+          <Typography variant="h2">Get to know me!</Typography>
+          <Typography variant='h5'>
+            I'm a Frontend Web Developer building the Front-end of Websites and
+            Web Applications that leads to the success of the overall product.
+            Check out some of my work in the Projects section. I also like
+            sharing content related to the stuff that I have learned over the
+            years in Web Development so it can help other people of the Dev
+            Community. Feel free to Connect or Follow me on my Linkedin where I
+            post useful content related to Web Development and Programming I'm
+            open to Job opportunities where I can contribute, learn and grow. If
+            you have a good opportunity that matches my skills and experience
+            then don't hesitate to contact me.
+          </Typography>
+        </Box>
+        <Box sx={{flex:1}}>
+          {/* <Box sx={{display:'flex', gap:1}}> */}
 
-export default About
+          {
+            skills.map((skill,index)=>{
+              return (
+                <Chip key={index} label={<Typography variant="h5" sx={{color:"white"}}>{skill}</Typography>}/>
+              )
+            })
+          }
+          {/* </Box> */}
+        </Box>
+      </Box>
+      <Box></Box>
+    </Box>
+  );
+};
+
+export default About;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
-import Header from "@app/component/header"
-import PersonalInfo from "@app/component/PersonalInfo"
+import About from "@app/component/About/About";
+import PersonalInfo from "@app/component/PersonalInfo";
+import Header from "@app/component/header";
 import { Box } from "@mui/material";
 export default function Home() {
     return (
@@ -8,7 +9,7 @@ export default function Home() {
                 <Header />
                 <PersonalInfo/>
             </Box>
-            <Box sx={{height:"100vh", background:"red"}} id="ABOUT"></Box>
+            <Box sx={{height:"100vh", background:"#3C4043"}} id="ABOUT"><About /></Box>
         </>
     );
 }
