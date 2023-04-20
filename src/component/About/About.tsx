d7dbbb155a3b8b9013cd56f8eeb22eab1a3e6aea
aquibAhmad <aquibahamd.vb@ensuredit.com>
2023-04-20 22:40:26 +0530

    added some chnages

--- src/component/About/About.tsx
+++ src/component/About/About.tsx
@@ -1,10 +1,10 @@
 /* eslint-disable react/no-unescaped-entities */
-import { Box, Chip, Divider, Typography } from '@mui/material';
+import { Box, Button, Chip, Divider, Typography } from '@mui/material';
 const skills = ["HTML",  "CSS",  "JavaScript",  "React",  "SASS",  "GIT",  "Github",  "Responsive Design",  "SEO",  "Terminal",  "Express Basics",  "SQL Basics"];
 
 const About = () => {
   return (
-    <Box sx={{height:"inherit"}}>
+    <Box sx={{height:"inherit", mx:25}}>
       <Box sx={{width: '100%'}}>
         <Typography
           variant='h1'
@@ -22,10 +22,10 @@ const About = () => {
           </Typography>
         </Box>
       </Box>
-      <Box sx={{ display: 'flex' ,flexDirection: { xs: 'column', sm: 'row' } }}>
+      <Box sx={{ display: 'flex' ,flexDirection: { xs: 'column', sm: 'row' }, columnGap:5 }}>
         <Box sx={{width:{xs:"100%", sm:"50%"}}}>
-          <Typography variant="h2">Get to know me!</Typography>
-          <Typography variant='h4'>
+          <Typography variant="h1"  sx={{py:2, color:"black"}}>Get to know me!</Typography>
+          <Typography variant='h4' sx={{wordBreak:"break-all", fontSize:"18px"}}>
             I'm a Frontend Web Developer building the Front-end of Websites and
             Web Applications that leads to the success of the overall product.
             Check out some of my work in the Projects section. I also like
@@ -37,18 +37,25 @@ const About = () => {
             you have a good opportunity that matches my skills and experience
             then don't hesitate to contact me.
           </Typography>
+          <Box sx= {{width:"100%", display:"flex",my:4, justifyContent:"flex-start"}}>
+                        <Button variant="contained" sx={{px:5, py:2}}>
+                            <Typography variant='h3'>CONTACT</Typography>
+                        </Button>
+                    </Box>
         </Box>
-        <Box sx={{width:{xs:"100%", sm:"50%"}}}>
-          {/* <Box sx={{display:'flex', gap:1}}> */}
+        <Box sx={{width:{xs:"100%", sm:"50%",}}}>
 
-          {
+        <Typography variant="h1"  sx={{py:2, color:"black"}}>My Skills</Typography>
+          <Box sx={{display:'flex', flexWrap: "wrap", gap:1}}>
+
+          { 
             skills.map((skill,index)=>{
               return (
-                <Chip key={index} label={<Typography variant="h5" sx={{color:"white"}}>{skill}</Typography>}/>
+                <Chip key={index} sx={{background:"black"}} label={<Typography variant="h5" sx={{color:"white"}}>{skill}</Typography>}/>
               )
             })
           }
-          {/* </Box> */}
+          </Box>
         </Box>
       </Box>
       <Box></Box>
