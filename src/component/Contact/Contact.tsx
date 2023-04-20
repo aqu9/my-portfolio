abceb1aa523d88942a4c15bf1de48f73543af982
rupesh.s <rupesh.s@valuebound.com>
2023-04-21 00:00:56 +0530

    add state in contact

--- src/component/Contact/Contact.tsx
+++ src/component/Contact/Contact.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { Box, Typography, Divider, Card, Container, TextField, Button } from '@mui/material';
 
 const styles = {
@@ -22,6 +22,19 @@ const styles = {
 };
 
 const Contact = () => {
+    const [formData, setFormData] = useState({ name: '', email: '', message: '' });
+
+    const {name, email, message} = formData;
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log(formData);
+    };
+
+    const handleChange = ({ target: { name, value } }: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData({ ...formData, [name]: value });
+      };
+
     return (
         <Box sx={{ height: 'inherit', mx: 25 }}>
             <Box>
@@ -36,6 +49,7 @@ const Contact = () => {
                 </Box>
                 <Box>
                     <Card sx={{ background: 'black', p: 2 }}>
+                        <form onSubmit={handleSubmit}>
                         <Container sx={{ display: 'flex', flexDirection: 'column', rowGap: 3 }}>
                             <Box>
                                 <Typography variant='h5' sx={{ color: 'white', py: 0.5 }}>
@@ -44,6 +58,8 @@ const Contact = () => {
                                 <TextField
                                     fullWidth
                                     autoComplete='off'
+                                    name="name"
+                                    value={name}
                                     type='text'
                                     sx={{ ...styles }}
                                     inputProps={{
@@ -56,6 +72,7 @@ const Contact = () => {
                                             color: 'white',
                                         },
                                     }}
+                                    onChange={handleChange}
                                 />
                             </Box>
                             <Box>
@@ -65,6 +82,8 @@ const Contact = () => {
                                 <TextField
                                     fullWidth
                                     autoComplete='off'
+                                    name="email"
+                                    value={email}
                                     type='text'
                                     sx={{ ...styles }}
                                     inputProps={{
@@ -77,6 +96,7 @@ const Contact = () => {
                                             color: 'white',
                                         },
                                     }}
+                                    onChange={handleChange}
                                 />
                             </Box>
                             <Box>
@@ -89,6 +109,8 @@ const Contact = () => {
                                     fullWidth
                                     rows={10}
                                     autoComplete='off'
+                                    name="message"
+                                    value={message}
                                     type='text'
                                     inputProps={{
                                         form: {
@@ -101,15 +123,21 @@ const Contact = () => {
                                             color: 'white',
                                         },
                                     }}
+                                    onChange={handleChange}
                                 />
                             </Box>
 
                             <Box sx={{ width: '100%', display: 'flex', my: 2, justifyContent: 'flex-end' }}>
-                                <Button variant='contained' sx={{ px: 5, py: 2 }}>
+                                <Button
+                                    variant='contained'
+                                    sx={{ px: 5, py: 2 }}
+                                    type='submit'
+                                >
                                     <Typography variant='h3'>SUBMIT</Typography>
                                 </Button>
                             </Box>
                         </Container>
+                        </form>
                     </Card>
                 </Box>
             </Box>
